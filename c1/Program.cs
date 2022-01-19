fa48710654fcfc072240c92d0041140571e116ee
MarcinZarzyczny <marcinzarzyczny1@gmail.com>
2022-01-19 19:35:48 +0100

    Object programming. Creation of point with coordinates x and y v3.

--- c1/Program.cs
+++ c1/Program.cs
@@ -5,34 +5,60 @@ namespace c1
     public class Punkt {
         public int x;
         public int y;
-        public void ustawWspulrzendne(int wspX, int wspY) {
-            this.x = wspX;
-            this.y = wspY;
-        }
-        public Punkt pobierzWspolrzedne()
+
+        public Punkt()
         {
+            ustawWspulrzendne(20, 52);
+
+        }
+        public Punkt(int x, int y)
+        {
+            ustawWspulrzendne(x, y);
+
+        }
+        public int getX() {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public void ustawWspulrzendne(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        public Punkt pobierzWspulrzendne () {
             Punkt punkt = new Punkt();
             punkt.x = x;
             punkt.y = y;
             return punkt;
+        }
+        public void pobierzWspulrzendne(Punkt punkt)
+        {
+            punkt.x = x;
+            punkt.y = y;
 
         }
-    
+        public void ustawX(int wspyX) {
+            x = wspyX;
+        }
+        public void ustawY(int wspyY)
+        {
+            y = wspyY;
+        }
     }
     class Program
     {
         public static void Main(string[] args)
         {
             Punkt punkt = new Punkt();
+            Console.WriteLine(punkt);
+            Console.WriteLine("Pubkt X: " + punkt.getX());
+            Console.WriteLine("Pubkt Y: " + punkt.getY());
 
-            Console.WriteLine("Przed ustawieniem wartości:");
-            Console.WriteLine("Wspułżędna X = " + punkt.pobierzWspolrzedne().x);
-            Console.WriteLine("Wspułżędna Y = " + punkt.pobierzWspolrzedne().y);
-
-            punkt.ustawWspulrzendne(1, 2);
-            Console.WriteLine("\nPo ustawieniu wartości: ");
-            Console.WriteLine("Wspułrzendna X = " + punkt.pobierzWspolrzedne().x);
-            Console.WriteLine("Wspułrzendna Y = " + punkt.pobierzWspolrzedne().y);
+            Punkt punkt1 = new Punkt(100, 200);
+            Console.WriteLine("Pubkt X: " + punkt1.getX());
+            Console.WriteLine("Pubkt Y: " + punkt1.getY());
 
 
 
