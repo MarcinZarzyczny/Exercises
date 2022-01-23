7b2a6c3a493f8899d4928e194689f5c715983e0f
MarcinZarzyczny <marcinzarzyczny1@gmail.com>
2022-01-23 20:56:52 +0100

    Object programming, array and foreach v2.

--- c1/Program.cs
+++ c1/Program.cs
@@ -6,10 +6,21 @@ namespace c1
     {
         public static void Main(string[] args)
         {
-            int[] tablica = { 1, 2, 3, 4, 5 };
-            foreach(int i in tablica) {
-                Console.WriteLine(i);
+            int[] tablica = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int iloscLiczbParzystych = 0;
+            int iloscLiczbNieparzystych = 0;
+            foreach (int i in tablica) {
+                
+                if (i % 2 == 0)
+                {
+                    iloscLiczbParzystych++;
+                }
+                else {
+                    iloscLiczbNieparzystych++;
+                }
             }
+            Console.WriteLine("Tablica zawiera {0} oraz {1}.", iloscLiczbParzystych, iloscLiczbNieparzystych);
+
         }
     }
 }
