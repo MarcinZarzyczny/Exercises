7531495f16ff8782994c17e6b089f218249d13e1
MarcinZarzyczny <marcinzarzyczny1@gmail.com>
2022-01-23 21:25:30 +0100

    Object programming, two-dimensional array.

--- c1/Program.cs
+++ c1/Program.cs
@@ -6,20 +6,24 @@ namespace c1
     {
         public static void Main(string[] args)
         {
-            int[] tablica = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            int iloscLiczbParzystych = 0;
-            int iloscLiczbNieparzystych = 0;
-            foreach (int i in tablica) {
-                
-                if (i % 2 == 0)
+            int[,] tablica = new int[2, 5];
+            int counter = 1;
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 5; j++)
                 {
-                    iloscLiczbParzystych++;
-                }
-                else {
-                    iloscLiczbNieparzystych++;
+                    tablica[i, j] = counter;
+                    counter++;
                 }
             }
-            Console.WriteLine("Tablica zawiera {0} oraz {1}.", iloscLiczbParzystych, iloscLiczbNieparzystych);
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("tablica[{0}, {1}] = {2}", i, j, tablica[i, j]);
+                }
+            }
+
+            
 
         }
     }
