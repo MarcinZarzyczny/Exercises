d88c0d16d97b3db5c4b69d9bcb8d60cd420a1d8d
MarcinZarzyczny <marcinzarzyczny1@gmail.com>
2022-01-24 20:37:05 +0100

    Object programming, the multidimensional array.

--- c1/Program.cs
+++ c1/Program.cs
@@ -6,21 +6,21 @@ namespace c1
     {
         public static void Main(string[] args)
         {
-            int[,] tablica = new int[2, 5];
-            int counter = 1;
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 5; j++)
-                {
-                    tablica[i, j] = counter;
-                    counter++;
-                }
-            }
-            for (int i = 0; i < 2; i++)
+            int[][] tablica = new int[4][];
+            tablica[0] = new int[4] { 1, 2, 3, 4 };
+            tablica[1] = new int[2] { 5, 6 };
+            tablica[2] = new int[3] { 7, 8, 9 };
+            tablica[3] = new int[1] { 10 };
+
+
+            for (int i = 0; i < tablica.Length; i++)
             {
-                for (int j = 0; j < 5; j++)
+                Console.Write("tablica [{0}] = ", i);
+                for (int j = 0; j < tablica[i].Length; j++)
                 {
-                    Console.WriteLine("tablica[{0}, {1}] = {2}", i, j, tablica[i, j]);
+                    Console.WriteLine("[{0}]", tablica[i][j]);
                 }
+                Console.WriteLine("");
             }
 
             
