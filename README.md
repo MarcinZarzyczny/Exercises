814ca4a3f7741367c40498c83313fab5930ef52d
MarcinZarzyczny <marcinzarzyczny1@gmail.com>
2022-02-06 20:43:45 +0100

    Object-oriented programing, interface cd.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # C # training repository #
-![Image](https://drive.google.com/file/d/1sYhL-u8jwSrV5SuHFSuzCN1Llal25pS0/view?usp=sharing)
--- c1/Program.cs
+++ c1/Program.cs
@@ -6,24 +6,77 @@ namespace c1
     {
         public static void Main(string[] args)
         {
-            Point Punkt = new Point();
-            Punkt.x = 100;
-            Punkt.y = 100;
-            Punkt.Show();
+
+           
+
+            Rectangle prostokant = new Rectangle(100, 200, 50, 50);
+            Point punkt1 = new Point(10, 20);
+            Point punkt2 = new Point(15, 18);
+            Point punkt3 = new Point(42, 24);
+            Triangle trojkant = new Triangle(punkt1, punkt2, punkt3);
+            prostokant.Show();
+            trojkant.Show();
+            punkt1.Show();
+            punkt2.Show();
+            punkt3.Show();
         }
     }
+
     interface IShow {
         void Show();  
     }
-    public class Point:IShow
-    {
+    public class Shape {
+        public int color;
+    
+    }
+    public class Rectangle: Shape, IShow {
         public int x;
         public int y;
-        public void Show() {
-            Console.WriteLine("Dane dotyczą punktu: \n");
-            Console.WriteLine("Współrzędna x = {0}", x);
-            Console.WriteLine("Współrzędna y = {0}", y);
+        public int width;
+        public int height;
+        public Rectangle(int x, int y, int width, int height) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
         }
-       
+        public void Show() {
+            Console.Write("\nParametry prostokąta");
+            Console.Write("\nwspółrzędna x = {0}", x);
+            Console.Write("\nwspółrzędna y = {0}", y);
+            Console.Write("\ndługość = {0}", width);
+            Console.Write("\nwysokość = {0}\n", height);
+        }
+    }
+    public class Triangle : Shape {
+        public Point a;
+        public Point b;
+        public Point c;
+        public Triangle(Point a, Point b, Point c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public void Show()
+        {
+            Console.Write("\nParametry trójkąta");
+            Console.Write("\npunkt a  = ({0}, {1})", a.x , a.y);
+            Console.Write("\npunkt b  = ({0}, {1})", b.x, b.y);
+            Console.Write("\npunkt a  = ({0}, {1})", c.x, c.y);
+            Console.Write("\n");
+        }
+    }
+    public class Point:Shape, IShow {
+        public int x;
+        public int y;
+
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        public void Show(){
+            Console.Write("punkt:\n x = {0}, y = {1}\n", this.x, this.y); ;
+        }
     }
 }
