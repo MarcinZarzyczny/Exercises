745ea991b64274264137ad98b313effe6c7ff98c
MarcinZarzyczny <marcinzarzyczny1@gmail.com>
2022-02-09 21:00:28 +0100

    Object-oriented programing, first graphical user interface.

---
+++ GUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    static class Program
+    {
+        class MainForm : Form
+        {
+            Button button = new Button();
+            public MainForm() {
+                this.Width = 320;
+                this.Height = 200;
+            }
+
+            [STAThread]
+            static void Main()
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                MessageBox.Show("Przykładowy tekst.");
+                Application.Run(new MainForm());
+            }
+        }
+    }
+}
